ab4789e1980f70562fa5a6eb4997c42eac1fa379
confucianzuoyuan <zuoyuantc@126.com>
2021-01-28 15:51:05 +0800

    第一次注释

--- kernel/main.c
+++ kernel/main.c
@@ -10,32 +10,32 @@ volatile static int started = 0;
 void
 main()
 {
-  if(cpuid() == 0){
-    consoleinit();
-    printfinit();
+  if(cpuid() == 0){ // 判断是否是第0个核
+    consoleinit(); // 初始化终端
+    printfinit(); // 初始化输出互斥锁
     printf("\n");
-    printf("xv6 kernel is booting\n");
+    printf("xv6 kernel is booting\n"); // 显示提示信息："xv6 kernel is booting\n"
     printf("\n");
-    kinit();         // physical page allocator
-    kvminit();       // create kernel page table
-    kvminithart();   // turn on paging
-    procinit();      // process table
-    trapinit();      // trap vectors
-    trapinithart();  // install kernel trap vector
-    plicinit();      // set up interrupt controller
-    plicinithart();  // ask PLIC for device interrupts
-    binit();         // buffer cache
-    iinit();         // inode cache
-    fileinit();      // file table
-    virtio_disk_init(); // emulated hard disk
-    userinit();      // first user process
-    __sync_synchronize();
+    kinit();         // physical page allocator 初始化物理内存页
+    kvminit();       // create kernel page table 创建内核页表
+    kvminithart();   // turn on paging 将h/w页表寄存器切换到内核的页表，并启用分页
+    procinit();      // process table 初始化进程表
+    trapinit();      // trap vectors 设置trap向量
+    trapinithart();  // install kernel trap vector 安装内核向量
+    plicinit();      // set up interrupt controller 设置中断控制器
+    plicinithart();  // ask PLIC for device interrupts 对S模式的hart设置uart启用位，及优先级阈值为0。注：hart指硬件线程。
+    binit();         // buffer cache 缓冲区初始化
+    iinit();         // inode cache inode缓冲区初始化
+    fileinit();      // file table 文件表初始化
+    virtio_disk_init(); // emulated hard disk 初始化虚拟磁盘
+    userinit();      // first user process 创建第一个用户进程。第一个进程执行一个小程序initcode.S（user/initcode.S:1），该程序通过调用exec系统调用重新进入内核
+    __sync_synchronize(); // 同步
     started = 1;
   } else {
-    while(started == 0)
+    while(started == 0) // 等待核0初始化完成
       ;
-    __sync_synchronize();
-    printf("hart %d starting\n", cpuid());
+    __sync_synchronize(); // 同步
+    printf("hart %d starting\n", cpuid()); // 输出核ID信息
     kvminithart();    // turn on paging
     trapinithart();   // install kernel trap vector
     plicinithart();   // ask PLIC for device interrupts
--- kernel/start.c
+++ kernel/start.c
@@ -8,6 +8,7 @@ void main();
 void timerinit();
 
 // entry.S needs one stack per CPU.
+// 每一个cpu都要有一个栈空间
 __attribute__ ((aligned (16))) char stack0[4096 * NCPU];
 
 // a scratch area per CPU for machine-mode timer interrupts.
@@ -17,35 +18,41 @@ uint64 timer_scratch[NCPU][5];
 extern void timervec();
 
 // entry.S jumps here in machine mode on stack0.
+// RISC-V的架构有机器模式（Machine Mode，M模式）、监督模式(Supervisor Mode，S模式）和用户模式（User Mode，U模式），并且规定机器模式是必须具备的模式，其他模式均是可选而非必选的模式。entry.S和start()函数都是在机器模式下执行。start对环境进行必要的配置后通过mret指令切换到监督模式，并执行main函数。
 void
 start()
 {
   // set M Previous Privilege mode to Supervisor, for mret.
-  unsigned long x = r_mstatus();
+  unsigned long x = r_mstatus(); // 读mstatus寄存器的值并保存在x中
   x &= ~MSTATUS_MPP_MASK;
   x |= MSTATUS_MPP_S;
   w_mstatus(x);
 
   // set M Exception Program Counter to main, for mret.
   // requires gcc -mcmodel=medany
+  // 将main的地址看作机器模式下发生异常时指令的地址并保存在寄存器mepc，当执行mret指令时，程序从发生异常的指令处恢复执行
   w_mepc((uint64)main);
 
   // disable paging for now.
+  // 将0写入页表寄存器satp来禁用虚拟地址转换
   w_satp(0);
 
   // delegate all interrupts and exceptions to supervisor mode.
+  // 将所有中断和异常委托给监督模式
   w_medeleg(0xffff);
   w_mideleg(0xffff);
   w_sie(r_sie() | SIE_SEIE | SIE_STIE | SIE_SSIE);
 
   // ask for clock interrupts.
+  // 对时钟芯片编程以产生计时器中断
   timerinit();
 
   // keep each CPU's hartid in its tp register, for cpuid().
-  int id = r_mhartid();
-  w_tp(id);
+  int id = r_mhartid(); // 读取核的ID
+  w_tp(id); // 将ID保存到tp寄存器中
 
   // switch to supervisor mode and jump to main().
+  // 执行mret指令，系统由机器模式改变为监督模式，并从main()开始运行
   asm volatile("mret");
 }
 
